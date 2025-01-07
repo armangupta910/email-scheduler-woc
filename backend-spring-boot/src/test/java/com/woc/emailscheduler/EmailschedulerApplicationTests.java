9bbd31ddc8ece36fbd925e3f01c4633637c526ce
armangupta910 <120269476+armangupta910@users.noreply.github.com>
2025-01-07 12:36:52 +0530

    Backend Template Code Added

---
+++ backend-spring-boot/src/test/java/com/woc/emailscheduler/EmailschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woc.emailscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
