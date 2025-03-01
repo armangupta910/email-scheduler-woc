db3f31b884986922a6e98003ad7f3e2892984299
Manisha Beniwal <b24ch1045@iitj.ac.in>
2025-03-01 13:06:31 +0530

    Create RegisterDto.java

---
+++ backend-spring-boot/src/main/java/com/woc/emailscheduler/model/RegisterDto.java
@@ -0,0 +1,62 @@
+package com.woc.emailscheduler.model;
+//reg
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class RegisterDto {
+    @NotEmpty
+    private String Name;
+    @NotEmpty
+    private String PhoneNumber;
+    @NotEmpty
+    private String Designation;
+    @NotEmpty
+    @Email
+    private String EmailId;
+    @Size (min=6, message = "Minimum password length is 6 characters.")
+    private String Password;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return EmailId;
+    }
+
+    public void setEmailId(String emailId) {
+        EmailId = emailId;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getDesignation() {
+        return Designation;
+    }
+
+    public void setDesignation(String designation) {
+        Designation = designation;
+    }
+
+
+}
